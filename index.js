6a2ac918c4a6326f0257c52eaa80d208ddedd3a7
Starlight Romero <starlightromero@gmail.com>
2017-11-28 16:50:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+funcstion logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.lof(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if () {
+  string === string.toLowerCase()
+
+  }
+}
